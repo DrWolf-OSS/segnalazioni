24696a488125ce1c4c068d2b431846cac29fba9d
Lorenzo Riganella <lorenzo.riganella@drwolf.com>
2013-04-11 14:53:34 +0200

    ricerca per segnalazione corrente

--- it.drwolf.alerting-ejb/ejbModule/it/drwolf/alerting/util/FotoUtils.java
+++ it.drwolf.alerting-ejb/ejbModule/it/drwolf/alerting/util/FotoUtils.java
@@ -1,6 +1,7 @@
 package it.drwolf.alerting.util;
 
 import it.drwolf.alerting.entity.Segnalazione;
+import it.drwolf.alerting.homes.SegnalazioneHome;
 
 import java.util.ArrayList;
 
@@ -8,6 +9,7 @@ import org.apache.chemistry.opencmis.client.api.ItemIterable;
 import org.apache.chemistry.opencmis.client.api.QueryResult;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.commons.data.PropertyData;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 @Name("fotoUtils")
@@ -15,13 +17,17 @@ public class FotoUtils {
 
 	CmisUtils cmisUtils = new CmisUtils();
 
+	@In
+	SegnalazioneHome segnalazioneHome;
+
 	@SuppressWarnings("unchecked")
 	public ArrayList getFoto(Segnalazione segnalazione) {
 		this.cmisUtils.login();
 		Session session = this.cmisUtils.getSession();
 		ItemIterable<QueryResult> results = session.query(
-				"select d.*, c.* from cmis:document as d join dw:caleearth as c on d.cmis:objectId  = c.cmis:objectId where c.dw:segnalazione = 1106", false);
-		System.out.println("ci passo");
+				"select d.cmis:objectId from cmis:document as d join dw:caleearth as c on d.cmis:objectId  = c.cmis:objectId where c.dw:segnalazione = "
+						+ this.segnalazioneHome.getInstance().getId(), false);
+		System.out.println("Cerco fotografie per segnalazione n° " + this.segnalazioneHome.getInstance().getId());
 
 		ArrayList resultList = new ArrayList();
 		for (QueryResult hit : results) {
