b0f811497db6f854e310851c8043137745090720
Lorenzo Riganella <lorenzo.riganella@drwolf.it>
2014-04-09 15:15:02 +0200

    aggiunto comando 'Pulisci gestori'

--- it.drwolf.alerting-ejb/ejbModule/it/drwolf/alerting/lists/TipoInterventoList.java
+++ it.drwolf.alerting-ejb/ejbModule/it/drwolf/alerting/lists/TipoInterventoList.java
@@ -1,9 +1,13 @@
 package it.drwolf.alerting.lists;
 
 import it.drwolf.alerting.entity.TipoIntervento;
+import it.drwolf.alerting.util.EloiseUtils;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
 
+import org.jboss.seam.Component;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.framework.EntityQuery;
 
@@ -17,8 +21,7 @@ public class TipoInterventoList extends EntityQuery<TipoIntervento> {
 	private TipoIntervento tipoIntervento = new TipoIntervento();
 
 	public TipoInterventoList() {
-		this.setRestrictionExpressionStrings(Arrays
-				.asList(TipoInterventoList.RESTRICTIONS));
+		this.setRestrictionExpressionStrings(Arrays.asList(TipoInterventoList.RESTRICTIONS));
 	}
 
 	@Override
@@ -33,6 +36,20 @@ public class TipoInterventoList extends EntityQuery<TipoIntervento> {
 
 	public TipoIntervento getTipoIntervento() {
 		return this.tipoIntervento;
+	}
+
+	public void pulisciGestori() {
+		EloiseUtils eu = (EloiseUtils) Component.getInstance("eloiseUtils");
+		Map<String, String> people = eu.getPeople();
+		for (TipoIntervento tipoIntervento : this.getResultList()) {
+			Iterator<String> i = tipoIntervento.getGestoriIntervento().iterator();
+			while (i.hasNext()) {
+				if (!people.values().contains(i.next())) {
+					i.remove();
+				}
+			}
+		}
+
 	}
 
 }
