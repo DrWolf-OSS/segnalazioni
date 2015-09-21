ee092f87acea0fcd359fe6b4dea8f19034fcb669
Andrea Agili <agea@drwolf.it>
2015-09-21 17:45:50 +0200

    bugfix invio servizi+gruppi

--- it.drwolf.iscrizioni-ejb/ejbModule/it/drwolf/iscrizioni/util/AsyncSender.java
+++ it.drwolf.iscrizioni-ejb/ejbModule/it/drwolf/iscrizioni/util/AsyncSender.java
@@ -1,12 +1,10 @@
 package it.drwolf.iscrizioni.util;
 
-import it.drwolf.iscrizioni.entity.AppParam;
-import it.drwolf.iscrizioni.entity.Iscritto;
-
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -19,6 +17,9 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.async.Asynchronous;
 
+import it.drwolf.iscrizioni.entity.AppParam;
+import it.drwolf.iscrizioni.entity.Iscritto;
+
 @Name("asyncSender")
 public class AsyncSender {
 
@@ -30,50 +31,45 @@ public class AsyncSender {
 
 	@SuppressWarnings("unchecked")
 	@Asynchronous
-	public void sendToAll(String from, String fromName, String subject,
-			String htmlBody, String textBody, String secret, String groups,
-			String services) throws Exception {
+	public void sendToAll(String from, String fromName, String subject, String htmlBody, String textBody, String secret,
+			String groups, String services) throws Exception {
 
-		AppParam port = this.entityManager.find(AppParam.class,
-				AppParam.APP_MAIL_PORT);
-		String mailHost = this.entityManager.find(AppParam.class,
-				AppParam.APP_MAIL_HOST).getValue();
-		String mailTrap = this.entityManager.find(AppParam.class,
-				AppParam.APP_MAIL_TRAP).getValue();
+		AppParam port = this.entityManager.find(AppParam.class, AppParam.APP_MAIL_PORT);
+		String mailHost = this.entityManager.find(AppParam.class, AppParam.APP_MAIL_HOST).getValue();
+		String mailTrap = this.entityManager.find(AppParam.class, AppParam.APP_MAIL_TRAP).getValue();
 
 		String mailUser = null;
 		try {
-			mailUser = this.entityManager.find(AppParam.class,
-					AppParam.APP_MAIL_USER).getValue();
+			mailUser = this.entityManager.find(AppParam.class, AppParam.APP_MAIL_USER).getValue();
 		} catch (Exception e1) {
 
 		}
 		String mailPass = null;
 		try {
-			mailPass = this.entityManager.find(AppParam.class,
-					AppParam.APP_MAIL_PASSWORD).getValue();
+			mailPass = this.entityManager.find(AppParam.class, AppParam.APP_MAIL_PASSWORD).getValue();
 		} catch (Exception e1) {
 
 		}
 
 		Query q = this.entityManager.createQuery("from Iscritto order by id");
 
+		Set<Iscritto> rcpt = new HashSet<Iscritto>();
+
 		if (groups != null) {
 			q = this.entityManager
-					.createQuery(
-							"select i from Iscritto i join i.gruppi g where g.id in (:l) group by i.id")
+					.createQuery("select i from Iscritto i join i.gruppi g where g.id in (:l) group by i.id")
 					.setParameter("l", Arrays.asList(groups.split(",")));
-		} else if (services != null) {
-			q = this.entityManager
-					.createQuery(
-							"select i from Iscritto i join i.opzioniServizi o where o.id in (:l) group by i.id")
-					.setParameter("l", Arrays.asList(services.split(",")));
+			rcpt.addAll(q.getResultList());
 		}
 
-		List<Iscritto> resultList = q.getResultList();
+		if (services != null) {
+			q = this.entityManager
+					.createQuery("select i from Iscritto i join i.opzioniServizi o where o.id in (:l) group by i.id")
+					.setParameter("l", Arrays.asList(services.split(",")));
+			rcpt.addAll(q.getResultList());
+		}
 
-		String text = from + fromName + subject + htmlBody + textBody
-				+ resultList + groups + services;
+		String text = from + fromName + subject + htmlBody + textBody + rcpt + groups + services;
 
 		String hash = DigestUtils.shaHex(text);
 
@@ -81,7 +77,7 @@ public class AsyncSender {
 			return;
 		} else {
 			this.mailMonitor.getInCorso().put(hash, new LinkedList<Iscritto>());
-			for (Iscritto i : resultList) {
+			for (Iscritto i : rcpt) {
 				this.mailMonitor.getInCorso().get(hash).add(i);
 			}
 		}
@@ -103,8 +99,7 @@ public class AsyncSender {
 					email.addTo(mailTrap);
 				} else {
 					if (i.getCognome() != null) {
-						email.addTo(i.getEmail(),
-								i.getNome() + " " + i.getCognome());
+						email.addTo(i.getEmail(), i.getNome() + " " + i.getCognome());
 					} else {
 						email.addTo(i.getEmail());
 					}
@@ -122,11 +117,9 @@ public class AsyncSender {
 					email.setSmtpPort(Integer.parseInt(port.getValue()));
 				}
 				email.send();
-				this.mailMonitor.setLastLog("Invio: " + new Date() + " - "
-						+ i.getEmail());
+				this.mailMonitor.setLastLog("Invio: " + new Date() + " - " + i.getEmail());
 			} catch (Exception e) {
-				this.mailMonitor.setLastLog("ERRORE Invio: " + new Date()
-						+ " - " + i.getEmail());
+				this.mailMonitor.setLastLog("ERRORE Invio: " + new Date() + " - " + i.getEmail());
 				System.out.println("Errore invio a " + i.getEmail());
 				e.printStackTrace();
 			}
