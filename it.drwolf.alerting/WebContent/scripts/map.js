ea5409dd710678c254ab4b3f5c8c09825e1eb461
Lorenzo Riganella <lorenzo.riganella@drwolf.it>
2016-02-04 15:03:37 +0100

    marker sulla posizione iniziale

--- it.drwolf.alerting/WebContent/scripts/map.js
+++ it.drwolf.alerting/WebContent/scripts/map.js
@@ -1,63 +1,84 @@
 
-      var marker;  
-      var map;
+var marker;  
+var map;
+
+var latField;
+var lngField;
 
 function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(43.865280, 11.167173),
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-   map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-    
-    
-	  geocoder = new google.maps.Geocoder();
+	latField = document.getElementById('mainform:inddec:lat');
+	lngField = document.getElementById('mainform:inddec:lng');
+	var mapOptions = {
+			center: getCenter(),
+			zoom: 14,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("map-canvas"),
+			mapOptions);
 
-      google.maps.event.addListener(map, 'click', function(event) {
-          placeMarker(event.latLng);
-      });
+	geocoder = new google.maps.Geocoder();
 
-      
-  }
+	google.maps.event.addListener(map, 'click', function(event) {
+		placeMarker(event.latLng); // aggiungo un marker
+		updateAddress(event.latLng); // aggiorno indirizzo nella pagina
+	});
+
+	//il posizionamento del marker iniziale deve essere l'ultima cosa.
+	if (latField.value > 0.0 && lngField.value > 0.0){
+		placeMarker(map.getCenter());
+	}
+
+}
+
+function getCenter(){
+	var center;
+	if (latField.value > 0.0 && lngField.value > 0.0){
+		center = new google.maps.LatLng(latField.value, lngField.value);
+	}
+	else{
+		center = new google.maps.LatLng(43.865280, 11.167173);		
+	}
+	return center;
+}
 
 
 
 
-      
-      function placeMarker(location) {
-          if(marker){ // se il marker esiste già
-              marker.setPosition(location); // si aggiorna la sua posizione
-          }else{
-              marker = new google.maps.Marker({ // se no si crea un nuovo marker
-                  position: location, 
-                  map: map
-              });
-          }
-          document.getElementById('mainform:inddec:lat').value=location.lat();
-          document.getElementById('mainform:inddec:lng').value=location.lng();
-          getAddress(location);           
-      }
-      
-      
-      
-      
-      function getAddress(latLng) {
-          geocoder.geocode( {'latLng': latLng},
-            function(results, status) {
-              if(status == google.maps.GeocoderStatus.OK) {
-                if(results[0]) {
-                	document.getElementById("mainform:inddec:pac-input").value = results[0].formatted_address;
-                }
-                else {
-                	alert("No results");
-                }
-              }
-              else {
-            	  alert(status);
-              }
-            });
-          }
-      
-      
+function placeMarker(location) {
+	if(marker){ // se il marker esiste già
+		marker.setPosition(location); // si aggiorna la sua posizione
+	}else{
+		marker = new google.maps.Marker({ // se no si crea un nuovo marker
+			position: location, 
+			map: map
+		});
+	}
+           
+}
+
+function updateAddress(location){
+	latField.value=location.lat();
+	lngField.value=location.lng();
+	getAddress(location);
+}
+
+
+function getAddress(latLng) {
+	geocoder.geocode( {'latLng': latLng},
+			function(results, status) {
+		if(status == google.maps.GeocoderStatus.OK) {
+			if(results[0]) {
+				document.getElementById("mainform:inddec:pac-input").value = results[0].formatted_address;
+			}
+			else {
+				alert("No results");
+			}
+		}
+		else {
+			alert(status);
+		}
+	});
+}
+
+
         
